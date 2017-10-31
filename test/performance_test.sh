8ba94fe1f96d3a958b463c27e72e7eb9c8283019
user404d <conduffql@gmail.com>
2017-10-31 14:57:35 -0500

    Update to write to tmp dir

--- test/performance_test.sh
+++ test/performance_test.sh
@@ -1,31 +1,40 @@
-#!/bin/bash
+#!/bin/bash -e
+
+PAILLIER=../../build/paillier
+TMP=tmp
+
+test -d ${TMP} || mkdir ${TMP}
+pushd ${TMP}
+trap popd ERR
 
 echo 3 > m1
 loops=10
 
 if [ ! -e pub1024 ] && [ ! -e priv1024 ]; then
 	echo 'Generating keys. This might take a while.'
-	./paillier keygen pub1024 priv1024 1024
+	./${PAILLIER} keygen pub1024 priv1024 1024
 fi
 echo "encryption performance, 1024 bits"
-time ./paillier encrypt c1 m1 pub1024
+time ./${PAILLIER} encrypt c1 m1 pub1024
 echo "decryption performance, 1024 bits"
-time ./paillier decrypt m2 c1 priv1024
+time ./${PAILLIER} decrypt m2 c1 priv1024
 
 if [ ! -e pub2048 ] && [ ! -e priv2048 ]; then
 	echo 'Generating keys. This might take a while.'
-	./paillier keygen pub2048 priv2048 2048
+	./${PAILLIER} keygen pub2048 priv2048 2048
 fi
 echo "encryption performance, 2048 bits"
-time ./paillier encrypt c1 m1 pub2048
+time ./${PAILLIER} encrypt c1 m1 pub2048
 echo "decryption performance, 2048 bits"
-time ./paillier decrypt m2 c1 priv2048
+time ./${PAILLIER} decrypt m2 c1 priv2048
 
 if [ ! -e pub4096 ] && [ ! -e priv4096 ]; then
 	echo 'Generating keys. This might take a while.'
-	./paillier keygen pub4096 priv4096 4096
+	./${PAILLIER} keygen pub4096 priv4096 4096
 fi
 echo "encryption performance, 4096 bits"
-time ./paillier encrypt c1 m1 pub4096
+time ./${PAILLIER} encrypt c1 m1 pub4096
 echo "decryption performance, 4096 bits"
-time ./paillier decrypt m2 c1 priv4096
+time ./${PAILLIER} decrypt m2 c1 priv4096
+
+popd
