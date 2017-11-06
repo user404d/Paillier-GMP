604d252349710e3779b2d2a20086a9f527c02703
user404d <conduffql@gmail.com>
2017-11-05 19:05:38 -0600

    Update to use std::algorithm functions

--- src/secure_dot_product.hpp
+++ src/secure_dot_product.hpp
@@ -1,6 +1,7 @@
 #ifndef PAILLIER_SECURE_DOT_PRODUCT_HPP
 #define PAILLIER_SECURE_DOT_PRODUCT_HPP
 
+#include <algorithm>
 #include <fstream>
 #include <functional>
 #include <gmpxx.h>
@@ -37,11 +38,7 @@ std::vector<U> map(std::vector<T> from, std::function<U(const T &)> f)
 {
     std::vector<U> to;
     to.reserve(from.size());
-    for (const auto &val : from)
-    {
-        const U result{f(val)};
-        to.push_back(result);
-    }
+    std::transform(from.begin(), from.end(), std::back_inserter(to), f);
     return to;
 }
 
@@ -50,32 +47,14 @@ std::vector<V> pairwise_map(const std::vector<T> &ts, const std::vector<U> &us, 
 {
     std::vector<V> vs;
     vs.reserve(std::min(ts.size(), us.size()));
-
-    auto ts_iter = ts.begin();
-    auto us_iter = us.begin();
-    auto ts_end = ts.end();
-    auto us_end = us.end();
-
-    while (ts_iter != ts_end && us_iter != us_end)
-    {
-        const V result{f(*ts_iter, *us_iter)};
-        vs.push_back(result);
-        ts_iter++;
-        us_iter++;
-    }
-
+    std::transform(ts.begin(), ts.end(), us.begin(), std::back_inserter(vs), f);
     return vs;
 }
 
 template <class T, class U>
 U reduce(const std::vector<T> &ts, std::function<U(const U &, const T &)> f, U init)
 {
-    U acc = init;
-    for (const auto &t : ts)
-    {
-        acc = f(acc, t);
-    }
-    return acc;
+    return std::accumulate(ts.begin(), ts.end(), init, f);
 }
 
 // sdp
