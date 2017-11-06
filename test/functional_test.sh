7105cfef4d15d21f88b71cab21930d02237b0547
user404d <conduffql@gmail.com>
2017-11-05 19:06:57 -0600

    Add test_result method for better test output

--- test/functional_test.sh
+++ test/functional_test.sh
@@ -5,11 +5,23 @@ U_VEC=u.vec
 V_VEC=v.vec
 RESULT=u_dot_v.out
 
+test_result()
+{
+    if [ "$1" -eq "$2" ]; then
+        echo "[+] -> $1 == $2"
+    else
+        echo "[-] -> $1 != $2"
+        if [ `head -n 2 $3 | tail -1` -le $2 ]; then
+            echo "output was larger than modulo n found in $3"
+        fi
+    fi
+}
+
 test -d ${TMP} || mkdir tmp
 echo "1 3 5 2 3 1 4" > ${TMP}/${U_VEC}
 echo "2 2 2 2 2 2 2" > ${TMP}/${V_VEC}
 
-./../bin/secure_dot_product --keygen 8 \
+./../bin/secure_dot_product --keygen 4096 \
     --pub ${TMP}/pub.key \
     --priv ${TMP}/priv.key \
     -u  ${TMP}/${U_VEC} \
@@ -18,4 +30,4 @@ echo "2 2 2 2 2 2 2" > ${TMP}/${V_VEC}
     --ev ${TMP}/v.vec.enc \
     --result ${TMP}/${RESULT}
 
-tail -1 ${TMP}/${RESULT}
+test_result `tail -1 ${TMP}/${RESULT}` "38" ${TMP}/pub.key
